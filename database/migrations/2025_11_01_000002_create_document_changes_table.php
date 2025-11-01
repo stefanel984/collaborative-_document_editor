6a9bf8a6d767f9d85f40b66b534a365d95f25368
stefanel984 <stefanel984@users.noreply.github.com>
2025-11-01 15:50:56 +0100

    Initial commit

---
+++ database/migrations/2025_11_01_000002_create_document_changes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+       Schema::create('document_changes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('document_id')->constrained('documents')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->json('operation'); // {insert:"text", position:5} or {delete: "text", position:3}
+            $table->integer('version'); // for Operational Transformation
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('document_changes');
+    }
+};
